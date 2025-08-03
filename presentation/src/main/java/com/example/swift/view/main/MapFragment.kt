ceab332d20983ee0c723ebd5e26cf3910a2789cd
park-hwaan <parkhwan1129@gmail.com>
2025-08-04 00:08:45 +0900

    feat: 바텀네비게이션 세팅

---
+++ presentation/src/main/java/com/example/swift/view/main/MapFragment.kt
@@ -0,0 +1,35 @@
+package com.example.swift.view.main
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.boombim.android.databinding.FragmentMapBinding
+import com.boombim.android.databinding.FragmentMyPageBinding
+
+class MapFragment : Fragment() {
+    private var _binding: FragmentMapBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentMapBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
